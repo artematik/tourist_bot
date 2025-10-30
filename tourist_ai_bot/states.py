834241875525416508adefbbeb825af2e4ab2dd2
NikitaNN052 <147333276+NikitaNN052@users.noreply.github.com>
2025-10-30 17:22:56 +0300

    Add files via upload

---
+++ tourist_ai_bot/states.py
@@ -0,0 +1,8 @@
+from aiogram.fsm.state import State, StatesGroup
+
+class UserState(StatesGroup):
+    interest = State()
+    time = State()
+    transport = State()
+    location = State()
+    awaiting_address_text = State()
